30917d3f505cdfff2c4a0c3e2b218a57e007c2f5
Hector Pulido <hector.pulido@platzi.com>
2023-05-07 18:56:56 -0600

    first tests

---
+++ peque_nlu/tests/test_saver.py
@@ -0,0 +1,28 @@
+import os
+from peque_nlu.intent_engines import SGDIntentEngine
+from peque_nlu.savers import PickleSaver
+
+PICKLE_PATH = "test.pkl"
+
+
+def test_save_pickle():
+    """
+    Test the save pickle function.
+    """
+    intent_engine = SGDIntentEngine()
+    saver = PickleSaver()
+    saver.save(intent_engine, PICKLE_PATH)
+    assert os.path.exists(PICKLE_PATH)
+    os.remove(PICKLE_PATH)
+
+
+def test_load_pickle():
+    """
+    Test the load pickle function.
+    """
+    intent_engine = SGDIntentEngine()
+    saver = PickleSaver()
+    saver.save(intent_engine, PICKLE_PATH)
+    intent_engine = saver.load(PICKLE_PATH)
+    assert intent_engine is not None
+    os.remove(PICKLE_PATH)
